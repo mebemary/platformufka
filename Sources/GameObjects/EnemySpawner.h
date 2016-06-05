c848635e6f7c496c3e9ea0c4b2aa8af711ea1e6b
mebemary <mariapolewczyk@gmail.com>
2016-06-05 23:06:15 +0200

    lolrand

--- Sources/GameObjects/EnemySpawner.h
+++ Sources/GameObjects/EnemySpawner.h
@@ -5,6 +5,7 @@
 #include "../GameObject.h"
 #include "Enemy.h"
 #include <random>
+#include <stdlib.h>
 
 struct EnemySpawnerState : public BaseState
 {
@@ -14,11 +15,11 @@ struct EnemySpawnerState : public BaseState
 
 class EnemySpawnerComponent : public Component<EnemySpawnerState>
 {
-        sf::Time nextInterval;;
+        sf::Time nextInterval;
         sf::Vector2f spawnPosition = { 900.0f, 400.0f };
-        std::random_device rd;
-        std::mt19937 randomGenerator;
-        std::uniform_real_distribution<> randomDistribution;
+        // std::random_device rd;
+        // std::mt19937 randomGenerator;
+        // std::uniform_real_distribution<> randomDistribution;
 
         std::shared_ptr<GameObject<EnemyState>> newEnemy()
         {
@@ -31,10 +32,11 @@ class EnemySpawnerComponent : public Component<EnemySpawnerState>
         }
 
     public:
-        EnemySpawnerComponent() :
-            rd(), randomGenerator(rd), randomDistribution(2, 4)
+        EnemySpawnerComponent() // :
+           // rd(), randomGenerator(rd), randomDistribution(2, 4)
         {
-            nextInterval = sf::seconds(randomDistribution(randomGenerator));
+            //nextInterval = sf::seconds(2 + (std::rand() / RAND_MAX) * 2); // sf::seconds(randomDistribution(randomGenerator));
+            nextInterval = sf::seconds(3);
         }
 
 	    void update(BaseState &spawnerStateBase, GameState &gameState)
@@ -45,7 +47,7 @@ class EnemySpawnerComponent : public Component<EnemySpawnerState>
 
             if (spawnerState.elapsed.asMilliseconds() >= spawnerState.lastSpawn.asMilliseconds() + nextInterval.asMilliseconds()) {
                 spawnerState.lastSpawn = spawnerState.elapsed;
-                nextInterval = sf::seconds(randomDistribution(randomGenerator));
+                //nextInterval = sf::seconds(2 + (std::rand() / RAND_MAX) * 2);// randomDistribution(randomGenerator));
 
                 gameState.addGameObject(newEnemy(), "enemy");
             }
